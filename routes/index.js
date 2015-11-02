d22ab385a5eefee08d8f721b80cfe5336f042ae4
bomatson <bobby@carbonfive.com>
2015-11-02 10:03:24 -0500

    wip

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Contract Reader' });
 });
+
+router.get('/submission', function(req, res, next) {
+  res.render('submission/new');
+});
+
+router.post('/submission', function(req, res, next) {
+  var link = req.body.contract_link;
+  request.post({url:'https://contract-reader.herokuapp.com/summarize', link: link}, function (error, resp, body) {
+    if (!error && resp.statusCode == 200) {
+      res.render('submission/show', { text: resp.text });
+    }
+  });
+});
+
 
 module.exports = router;
